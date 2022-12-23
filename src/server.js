1773daa32452ee892bddb2db79baf9e9fbb777de
Mirmuhsin <99722067+DiabloDraco@users.noreply.github.com>
2022-12-23 23:58:17 +0500

    added cors

--- src/server.js
+++ src/server.js
@@ -4,14 +4,15 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
-app.use(cors( ))
-app.use(cors('*'))
-app.use(express.static(path.join(__dirname,'./uploads')))
+app.use(cors(corsOptions))
+var corsOptions = {
+    origin: 'https://hillhouse-capital.org'
+}
+app.use(express.static(path.join(__dirname, './uploads')))
 
 const appRouter = require('./modules/index.js')
 app.use(appRouter)
 
 
 
-app.listen(process.env.PORT||5555,() =>console.log('server run'))
+app.listen(process.env.PORT || 5555, () => console.log('server run'))
