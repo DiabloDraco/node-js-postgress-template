fa4ff0644b7a150bdaecaad1e65f08aa35d32533
DiabloDraco~ <mirmuhsinmirahmatov@gmail.com>
2023-03-15 16:14:30 +0500

    add

--- src/server.js
+++ src/server.js
@@ -1,34 +1,32 @@
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
-const app = express()
-app.use(express.json())
-var whitelist = ['https://hillhouse-capital.org']
+const app = express();
+app.use(express.json());
+var whitelist = ["https://hillhouse-capital.org"];
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error("Not allowed by CORS"));
     }
-  }
-}
-
-app.use(cors(corsOptions))
-app.use(express.static(path.join(__dirname, './uploads')))
+  },
+};
+// add
+app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, "./uploads")));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
 
-const appRouter = require('./modules/index.js')
-app.use(appRouter)
+const appRouter = require("./modules/index.js");
+app.use(appRouter);
 
-
-
-app.listen(process.env.PORT || 5555, () => console.log('server run'))
+app.listen(process.env.PORT || 5555, () => console.log("server run"));
