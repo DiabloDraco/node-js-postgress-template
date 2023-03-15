7cb93bb8f2ce10829fb247ce10a33b27f8b9f6d2
DiabloDraco~ <mirmuhsinmirahmatov@gmail.com>
2023-03-15 17:52:03 +0500

    add

--- src/server.js
+++ src/server.js
@@ -4,18 +4,9 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-var whitelist = ["https://hillhouse-capital.org"];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+
 // add
-app.use(cors(corsOptions));
+app.use(cors("*"));
 app.use(express.static(path.join(__dirname, "./uploads")));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
